214b3c9ffa5403d84038dac8bc16b2c169aefd9a
trsaints <thiago.dev2k3@gmail.com>
2022-08-22 21:52:44 -0300

    Alteração para chave API de teste

--- app/src/services/projects_service.js
+++ app/src/services/projects_service.js
@@ -1,14 +1,13 @@
 async function getProjects() {
   const binURL = "https://api.jsonbin.io/v3/b/62f018c4a1610e6386f31ba6";
-  const apiKey = process.env.X_MASTER_KEY;
+  const apiKey = "$2b$10$qLrImDMxi7lj.EcgVEzTHuGGKkZpuYIeHTAzICfDscmwK3vpr0ns.";
 
   const response = await fetch(binURL, {
     method: "GET",
     headers: {
-      "X-Master-Key":
-        `${apiKey}`,
+      "X-Access-Key": `${apiKey}`,
       "X-Bin-Meta": false,
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
   });
 
