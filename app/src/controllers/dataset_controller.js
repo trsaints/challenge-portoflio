0e039fe41e5f4df94e8bb82fea7dae54c2a7b0b1
trsaints <thiago.dev2k3@gmail.com>
2023-02-08 22:23:29 -0300

    Controladores desnecessários

--- app/src/controllers/dataset_controller.js
+++
@@ -1,9 +0,0 @@
-function setDataLink() {
-  const navlinks = document.querySelector("[data-element='nav-links']");
-
-  for (let child of navlinks.children) {
-    child.dataset.element = "link";
-  }
-}
-
-export const datasetController = { setDataLink };
